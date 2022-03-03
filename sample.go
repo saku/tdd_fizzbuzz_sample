acad49d2aab55f72f40ff9515d223b668ceb4b47
Yoichiro Sakurai <saku2saku@gmail.com>
2022-03-03 18:35:47 +0900

    Add buzz rule.

--- sample.go
+++ sample.go
@@ -1,5 +1,14 @@
 package tddbc
 
+import "fmt"
+
 func Say(greeting string) string {
 	return greeting + " TDD BootCamp!!"
+}
+
+func Fizzbuzz(v int) string {
+	if v%3 == 0 {
+		return "fizz"
+	}
+	return fmt.Sprint(v)
 }
--- sample_test.go
+++ sample_test.go
@@ -1,8 +1,9 @@
 package tddbc
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSay(t *testing.T) {
@@ -17,4 +18,11 @@ func TestSay(t *testing.T) {
 func TestSay_testify(t *testing.T) {
 	actual := Say("Hello!")
 	assert.Equal(t, "Hello! TDD BootCamp!!", actual, "they should be equal")
+}
+
+func TestFizzbuzz(t *testing.T) {
+	assert.Equal(t, "1", Fizzbuzz(1), "1")
+	assert.Equal(t, "2", Fizzbuzz(2), "2")
+	assert.Equal(t, "fizz", Fizzbuzz(3), "3")
+	assert.Equal(t, "buzz", Fizzbuzz(5), "5")
 }
