e24025f8ecac3b314f71c1a19f2c668c48999acb
Yoichiro Sakurai <saku2saku@gmail.com>
2022-03-08 11:53:03 +0900

    Add implementation.

--- domain/model.go
+++ domain/model.go
@@ -1,5 +1,7 @@
 package domain
 
+import "fmt"
+
 type FizzBuzz struct {
 	num uint
 }
@@ -8,6 +10,15 @@ func NewFizzBuzz(num uint) *FizzBuzz {
 	return &FizzBuzz{num: num}
 }
 
-func (f FizzBuzz) Fizzbuzz() string {
-	return ""
+func (f *FizzBuzz) Fizzbuzz() string {
+	if f.num%15 == 0 {
+		return "fizzbuzz"
+	}
+	if f.num%5 == 0 {
+		return "buzz"
+	}
+	if f.num%3 == 0 {
+		return "fizz"
+	}
+	return fmt.Sprint(f.num)
 }
