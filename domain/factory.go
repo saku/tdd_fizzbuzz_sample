d11918879fa332d844510a3f29bad45254def370
Yoichiro Sakurai <saku2saku@gmail.com>
2022-03-08 12:01:52 +0900

    Add implementation.

--- domain/factory.go
+++ domain/factory.go
@@ -2,10 +2,10 @@ package domain
 
 type FizzBuzzFactory struct{}
 
-func NewFizzBuzzFactory() FizzBuzzFactory {
-	return FizzBuzzFactory{}
+func NewFizzBuzzFactory() *FizzBuzzFactory {
+	return &FizzBuzzFactory{}
 }
 
-func (factory FizzBuzzFactory) Create(num uint) FizzBuzz {
+func (factory FizzBuzzFactory) Create(num uint) *FizzBuzz {
 	return NewFizzBuzz(1)
 }
