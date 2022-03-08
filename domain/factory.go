4eaf87730a8127f75a1740378160f7d0e73b6d95
Yoichiro Sakurai <saku2saku@gmail.com>
2022-03-08 12:40:46 +0900

    Update impl.

--- domain/factory.go
+++ domain/factory.go
@@ -1,5 +1,7 @@
 package domain
 
+import "fmt"
+
 type FizzBuzzFactory struct{}
 
 func NewFizzBuzzFactory() *FizzBuzzFactory {
@@ -7,5 +9,8 @@ func NewFizzBuzzFactory() *FizzBuzzFactory {
 }
 
 func (factory FizzBuzzFactory) Create(num uint) (*FizzBuzz, error) {
+	if num < 1 || 100 < num {
+		return nil, fmt.Errorf("invalid number")
+	}
 	return NewFizzBuzz(num), nil
 }
